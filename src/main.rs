d948baf0842a03fffbc2a9655465e63bc1a0b01d
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-01 21:27:07 +0700

    a functioning calculator now
    its a calculator now
    it actually works

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,58 @@ use std::io::Write;
 // use std::io::ErrorKind;
 
 fn main() {
+    println!("===[Calculator]===");
     loop {
-        print!("Enter a number: ");
+        print!("Enter first number: ");
 
+        let mut firstnum = String::new();
         io::stdout().flush().unwrap();
-        let mut input = String::new();
         io::stdin()
-            .read_line(&mut input)
+            .read_line(&mut firstnum)
             .expect("Failed to read line.");
         
-        let convert: i32 = match input.trim().parse(){
+        let firstnum: i32 = match firstnum.trim().parse(){
             Ok(num) => num,
             Err(_) => {
                 println!("Not a number!");
                 continue;
             },
         };
+
+        let mut operator = String::new();
+        print!("Enter operator: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut operator)
+            .expect("Failed to read line.");
+
+        let mut secondnum = String::new();
+        print!("Enter second number: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut secondnum)
+            .expect("Failed to read line.");
+        
+        let secondnum: i32 = match secondnum.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number!");
+                continue;
+            },
+        };
+        //types of operators
+        if operator.trim() == "+" {
+            println!("{} + {} = {}", firstnum, secondnum, firstnum + secondnum);
+        } else if operator.trim() == "-" {
+            println!("{} - {} = {}", firstnum, secondnum, firstnum - secondnum);
+        } else if operator.trim() == "*" {
+            println!("{} * {} = {}", firstnum, secondnum, firstnum * secondnum);
+        } else if operator.trim() == "/" {
+            println!("{} / {} = {}", firstnum, secondnum, firstnum / secondnum);
+        } else {
+            println!("Invalid operator!");
+        }
         // let gotint = parsetou32(input);
-        println!("You entered: {convert}");
     }
 }
 
