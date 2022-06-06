a3aff9857a21fe0b2d937408959c1e3ba6170c3a
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-06 21:31:36 +0700

    it worked!
    yep, it finally worked.

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,28 @@ fn main() {
         vec.pop();
         vec.pop();
         vec.remove(0);
-        let mut vec2: Vec<&str> = vec.clone();
+        let vec2: Vec<&str> = vec.clone();
         let mut first_num = String::new();
         let mut second_num = String::new();
         let mut operator = String::new();
-        let mut index;
+        let mut index = 0;
         for i in vec {
             if is_real_num(i) {
                 // let s = i.to_string();
                 first_num = first_num + i;
-                println!("{} is a real number.", i);
+                // println!("{} is a real number.", i);
             } else {
                 if i == "+" || i == "-" || i == "*" || i == "/" {
                     operator = i.to_string();
-                    println!("{} is an operator.", operator);
+                    // println!("{} is an operator.", operator);
                     index = vec2.iter().position(|&r| r == i).unwrap();
-                    println!("index : {}", index);
+                    // println!("index : {}", index);
                     break;
-                } else {
+                }
+                else if i == "."{
+                    first_num = first_num + i;
+                } 
+                else {
                     if i == " "{
                         continue;
                     }else{
@@ -45,10 +49,35 @@ fn main() {
                 }
             }  
         }
-        for i in vec2 {
-            if i == operator {
-                println!("operator : {}", i);
-            }
+        let secnumvec: Vec<&str> = (&vec2[index + 1..]).to_vec();
+        for i in secnumvec{
+            second_num = second_num + i;
         }
+        let first_num = match first_num.parse::<f64>() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+        let second_num = match second_num.parse::<f64>() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+        let mut res;
+        if operator == "+" {
+            res = first_num + second_num;
+        } else if operator == "-" {
+            res = first_num - second_num;
+        } else if operator == "*" {
+            res = first_num * second_num;
+        } else if operator == "/" {
+            res = first_num / second_num;
+        } else {
+            println!("{} is not a valid operator.", operator);
+            continue;
+        }
+        println!("Result : {}", res);
     }
 }
