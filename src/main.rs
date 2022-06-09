e0cf374fc4cf4eff8abc41b8ca6a7d6b88b5e3c9
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-09 22:03:20 +0700

    read\write
    testing both read and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-// use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::io::Read;
 // use std::io::Write;
-// use std::env;
-use std::fs;
-// mod num;
-// use num::is_real_num;
-// use core::ptr::null;
-// use std::io::ErrorKind;
+// use std::fs;
+
 
 fn main() {
-    let fileName = "file.txt";
-    println!("in file : {}", fileName);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open("file.txt");
     
-    let read = fs::read_to_string(fileName)
-        .expect("Something went wrong reading the file");
-    println!("text : \n{}", read.trim());
+    dbg!(file);
+    println!("OpenOptions");
+    let mut file = File::create("file.txt").unwrap();
+    dbg!(&file);
+
+    println!("File::create");
+    
+    let mut file_contents = String::new();
+    
+    println!("file.write_all");
+    
+    file.write_all("test message waow2".as_bytes())
+        .expect("unable to write file");
+    
+    println!("file.read_to_string");
+    let mut file = File::open("file.txt").unwrap();
+    file.read_to_string(&mut file_contents).expect("unable to read file");
+
+    println!("current content: \n{}", file_contents);
 }
