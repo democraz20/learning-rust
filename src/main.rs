98b98f4ac177c0b1fad891036e1de597b8abb330
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-11 01:36:14 +0700

    it all works now
    yep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-// use std::fs::File;
+use std::fs::File;
 // use std::io::prelude::*;
 // use std::fs::OpenOptions;
-// use std::io::Read;
+use std::io::Read;
 // use std::io::Write;
 // use std::fs;
 extern crate serde_json;
@@ -9,36 +9,54 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-use serde_json::Value as json_value;
+// use serde_json::Value as json_value;
 
 #[derive(Serialize, Deserialize)]
-struct data {
+struct Data {
     name: String,
     id: u32,
-    ops: Vec<String>
+    ops: Vec<Ops>
 }
 
-// #[derive(Serialize, Deserialize)]
-// struct data_list {
-//     data: Vec<>
-// }
-
+#[derive(Serialize, Deserialize)]
+struct Ops {
+    name: String,
+    id: u32
+}
 
 fn main() {
-    let json = r#"
-    {
-        "name" : "Admin",
-        "id" : 1,
-        "ops" : ["1", "2", "3"]
-    }"#;
-    let res = serde_json::from_str(json);
+    // let json = r#"
+    // {
+    //     "name" : "Admin",
+    //     "id" : 1,
+    //     "ops" : [
+    //         {
+    //             "name" : "Add",
+    //             "id" : 1
+    //         },
+    //         {
+    //             "name" : "Delete",
+    //             "id" : 2
+    //         },
+    //         {
+    //             "name" : "Update",
+    //             "id" : 3
+    //         }
+    //     ]
+    // }"#;
+    let mut json = String::new();
+    
+    let mut file = File::open("main.json").unwrap();
+    file.read_to_string(&mut json).expect("unable to read file");
 
+    let res = serde_json::from_str(&json);
+    let index = 1;
     if res.is_ok(){
-        let parsed: data = res.unwrap();
+        let parsed: Data = res.unwrap();
         println!("name : {}", parsed.name);
         // println!("ops  : {:?}", parsed["ops"].as_array().unwrap());
         println!("id   : {}", parsed.id);
-        println!("ops  : {:?}", parsed.ops);
+        println!("ops-name  : {}, ops-id : {}", parsed.ops[index].name, parsed.ops[index].id);
     } else {
         println!("error : could not parse json");
     }
