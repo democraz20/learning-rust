6bfe682a5260245ed769bf4f7cad6d2e109d8938
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-11 11:23:57 +0700

    test appending to vec
    append to json vector and write to file successfully

--- src/main.rs
+++ src/main.rs
@@ -46,17 +46,38 @@ fn main() {
     // }"#;
     let mut json = String::new();
     
+    let test_append = r#"
+    {
+        "name" : "test_ops",
+        "id" : 4
+    }"#;
+
     let mut file = File::open("main.json").unwrap();
     file.read_to_string(&mut json).expect("unable to read file");
 
     let res = serde_json::from_str(&json);
+    let test_ops = serde_json::from_str(&test_append);
+    let parsed_test: Ops = test_ops.unwrap();
     let index = 1;
     if res.is_ok(){
-        let parsed: Data = res.unwrap();
+        let mut parsed: Data = res.unwrap();
         println!("name : {}", parsed.name);
-        // println!("ops  : {:?}", parsed["ops"].as_array().unwrap());
         println!("id   : {}", parsed.id);
         println!("ops-name  : {}, ops-id : {}", parsed.ops[index].name, parsed.ops[index].id);
+        
+        parsed.ops.push(parsed_test);
+        for i in &parsed.ops{
+            println!("{}, {}", i.name, i.id);
+        }
+        match std::fs::write(
+            "test.json",
+            serde_json::to_string_pretty(&parsed).unwrap()
+        ) 
+        {
+            Ok(_) => println!("Success!"),
+            Err(e) => println!("Error: {}", e),
+        }
+        // parsed.ops.pop();
     } else {
         println!("error : could not parse json");
     }
