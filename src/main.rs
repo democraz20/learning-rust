c328d4cb5b61fd94bf5261ccee43752471736bd3
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-06-29 20:57:34 +0700

    successful themes
    only with buttons for now

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ struct Counter{
     increment_button: button::State,
     decrement_button: button::State,
     alert_message: String,
+    theme: style::Theme,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -42,7 +43,7 @@ impl Sandbox for Counter {
                 Button::new(&mut self.increment_button, Text::new("+"))
                 .on_press(Message::IncrementPressed)
                 .padding([10, 50])
-                
+                .style(self.theme)
             )
             .push(
                 Text::new(self.value.to_string()).size(50),
@@ -51,14 +52,21 @@ impl Sandbox for Counter {
                 Button::new(&mut self.decrement_button, Text::new("-"))
                 .on_press(Message::DecrementPressed)
                 .padding([10, 50])
+                .style(self.theme)
             )
             .push(
                 Text::new(self.alert_message.to_string()).size(50),
             )
+            // .style(self.theme)
             .align_items(Alignment::Center);
-            // .style();
         
-        let container1 = container::Container::new(content).width(Length::Fill).height(Length::Fill).center_y().center_x().into();
+        let container1 = container::Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .center_x()
+            .style(self.theme)
+            .into();
         // let container1: container = container1.container.Container.Style.background(Color:: BLACK);
         // let container1 = container::Container::new(content).width(Length::Fill).center_x().into();
         // container1.Style.background(Color::BLACK)
@@ -77,6 +85,9 @@ impl Sandbox for Counter {
         }
     }
 }
+
+
+#[allow(unused_must_use)]
 mod style {
     use iced::{
         button, checkbox, container, progress_bar, radio, rule, scrollable,
@@ -96,6 +107,123 @@ mod style {
     impl Default for Theme {
         fn default() -> Theme {
             Theme::Dark
+        }
+    }
+
+    impl<'a> From<Theme> for Box<dyn container::StyleSheet + 'a> {
+        fn from(theme: Theme) -> Self {
+            match theme {
+                Theme::Light => Default::default(),
+                Theme::Dark => dark::Container.into(),
+            }
+        }
+    }
+    
+    impl<'a> From<Theme> for Box<dyn button::StyleSheet + 'a> {
+        fn from(theme: Theme) -> Self {
+            match theme {
+                Theme::Light => light::Button.into(),
+                Theme::Dark => dark::Button.into(),
+            }
+        }
+    }
+
+    mod light {
+        use iced::{button, Color, Vector};
+
+        pub struct Button;
+
+        impl button::StyleSheet for Button {
+            fn active(&self) -> button::Style {
+                button::Style {
+                    background: Color::from_rgb(0.11, 0.42, 0.87).into(),
+                    border_radius: 12.0,
+                    shadow_offset: Vector::new(1.0, 1.0),
+                    text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
+                    ..button::Style::default()
+                }
+            }
+
+            fn hovered(&self) -> button::Style {
+                button::Style {
+                    text_color: Color::WHITE,
+                    shadow_offset: Vector::new(1.0, 2.0),
+                    ..self.active()
+                }
+            }
+        }
+    }
+
+    #[allow(unused_must_use)]
+    mod dark {
+        use iced::{
+            button, checkbox, container, progress_bar, radio, rule, scrollable,
+            slider, text_input, toggler, Color,
+        };
+
+        const SURFACE: Color = Color::from_rgb(
+            0x40 as f32 / 255.0,
+            0x44 as f32 / 255.0,
+            0x4B as f32 / 255.0,
+        );
+
+        const ACCENT: Color = Color::from_rgb(
+            0x6F as f32 / 255.0,
+            0xFF as f32 / 255.0,
+            0xE9 as f32 / 255.0,
+        );
+
+        const ACTIVE: Color = Color::from_rgb(
+            0x72 as f32 / 255.0,
+            0x89 as f32 / 255.0,
+            0xDA as f32 / 255.0,
+        );
+
+        const HOVERED: Color = Color::from_rgb(
+            0x67 as f32 / 255.0,
+            0x7B as f32 / 255.0,
+            0xC4 as f32 / 255.0,
+        );
+
+        pub struct Container;
+
+        impl container::StyleSheet for Container {
+            fn style(&self) -> container::Style {
+                container::Style {
+                    background: Color::from_rgb8(0x36, 0x39, 0x3F).into(),
+                    text_color: Color::WHITE.into(),
+                    ..container::Style::default()
+                }
+            }
+        }
+
+        pub struct Button;
+
+        impl button::StyleSheet for Button {
+            fn active(&self) -> button::Style {
+                button::Style {
+                    background: ACTIVE.into(),
+                    border_radius: 3.0,
+                    text_color: Color::WHITE,
+                    ..button::Style::default()
+                }
+            }
+
+            fn hovered(&self) -> button::Style {
+                button::Style {
+                    background: HOVERED.into(),
+                    text_color: Color::WHITE,
+                    ..self.active()
+                }
+            }
+
+            fn pressed(&self) -> button::Style {
+                button::Style {
+                    border_width: 1.0,
+                    border_color: Color::WHITE,
+                    ..self.hovered()
+                }
+            }
         }
     }
 }
