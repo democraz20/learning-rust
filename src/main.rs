7ee82b4a5fef8ca23319b9f654d9d05107067f80
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-07-09 01:44:22 +0700

    system logging

--- src/main.rs
+++ src/main.rs
@@ -1,178 +1,72 @@
-// #[macro_use] extern crate prettytable;
-use prettytable::{
-    // color,
-    // Attr,
-    Cell,
-    Row,
-    Table,
-};
-use serde::{
-    Deserialize, 
-    Serialize
-};
+use std::{thread, time};
 
-use serde_json::Result;
-// use std::env;
-use std::fs;
-use std::io;
-use std::io::stdout;
-use std::io::Write;
-use std::env;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
 
-#[derive(Serialize, Deserialize)]
-struct JsonData {
-    table: String,
-    desc: String,
-    config: Config,
-    data: Vec<Item>,
-}
+use chrono::prelude::*;
 
 #[allow(unused_imports)]
-use crossterm::{
-    execute,
-    style::{
-        Color, 
-        Print, 
-        ResetColor, 
-        SetBackgroundColor, 
-        SetForegroundColor
-    },
-    ExecutableCommand,
-    event,
-};
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    title_style: String,
-    index_style: String
-}
+#[allow(dead_code)]
+const DURATION: std::time::Duration = time::Duration::from_secs(1200);
+const FILENAME: &str = "logs.txt";
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Item {
-    username: String,
-    password: String,
-    status: bool,
-    finished: bool,
-}
-
-#[allow(unused_must_use, unused_variables)]
-// fn main() -> Result<()> {
-fn main() {
-    // Create the table
-    // let mut num: i32 = 1;
-    let filename = String::from("main.json");
-
+fn main() -> std::io::Result<()> {
+    let mut sys = System::new_all();
     loop {
-        let current_dir = env::current_dir().unwrap();
-        // print!("{}$", current_dir.display());
-        execute!(
-            stdout(),
-            SetForegroundColor(Color::Green),
-            Print(format!("{}", current_dir.display())),
-            ResetColor
-        );
-        print!("$ ");
-        // execute!(
-        //     stdout(),
-        //     Set
-        // );
-        let mut input = String::new();
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line.");
-        dbg!(&input);
-        input.pop();
-        println!("[debug] pop occurs");
-        dbg!(&input);
-        // input = input.to_string().trim();
-        let commands = input.split(" ");
-        let cmd = commands.collect::<Vec<&str>>();
-        dbg!(&cmd);
-        let lenght = cmd.len();
-        // println!("{:?}", cmd);
-        let main_cmd = cmd[0].to_lowercase();
-        if main_cmd == "printtb" {
-            print_table(&filename);
-        }
-        
-        //defaults
-        else if main_cmd == "exit" { break;
-        } else if main_cmd == "" {}
-        else {println!("Unrecognized command : {}", main_cmd);}; //might revision
-    }
-        
-    // Ok(())
-}
+        sys.refresh_all();
+        println!("=> system:");
+        // RAM and swap information:
+        let total_mem: f32 = sys.total_memory() as f32 / 1048576.0;
+        let used_mem: f32 = sys.used_memory() as f32 / 1048576.0;
+        let total_swap: f32 = sys.total_swap() as f32 / 1048576.0;
+        let used_swap: f32 = sys.used_swap() as f32 / 1048576.0;
 
-// fn print_table
-fn print_table(filename: &str) -> Result<()> {
-    let mut table = Table::new();
-    
+        print!("total memory: {} GB", total_mem);
+        print!(", used memory: {} GB", used_mem);
+        print!(", total swap: {} GB", total_swap);
+        println!(", used swap: {} GB", used_swap);
 
-    // let contents = fs::read_to_string(filename)
-    //     //
-    //     .expect("Something went wrong reading th e file");
+        let local = Local::now();
+        let local_time = local.to_string();
+        let local_time = local_time.split(" ");
+        let mut local_time = local_time.collect::<Vec<&str>>();
+        local_time.pop();
+        println!("Current time : {:?}", local_time);
 
-    // let data: JsonData = serde_json::from_str(&contents).unwrap();
-    let data = parse(filename);
+        let final_write = format!(
+            "{}, {}, {}, {} \n  Logged At : {:?}",
+            total_mem, used_mem, total_swap, used_swap, local_time
+        )
+        .to_string();
 
-    let title_style = data.config.title_style;
-    let index_style = data.config.index_style;
-    println!("{}, {}", data.table, data.desc);
-    // for i in data.data.iter() {
-    //     print!("data : {:?}", i);
-    //     println!();
-    // }
-    println!();
-    // Add a row per time
-    table.add_row(Row::new(vec![
-        Cell::new("Index").style_spec(&title_style),
-        Cell::new("Username").style_spec(&title_style),
-        Cell::new("Password").style_spec(&title_style),
-        Cell::new("Status").style_spec(&title_style),
-        Cell::new("finished").style_spec(&title_style),
-    ]));
-    #[allow(unused_variables)]
-    for (i, el) in data.data.iter().enumerate() {
-        // print!("data : {:?}, index : {}", el, i);
-        // println!();
-        let mut _status_style;
-        let mut _finished_style;
-        if data.data[i].status {
-            _status_style = "bFg";
+        if Path::new(FILENAME).exists() {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(FILENAME)
+                .unwrap();
+            if let Err(e) = writeln!(file, "{}", &final_write) {
+                eprintln!("Couldn't write to file: {}", e);
+            }
         } else {
-            _status_style = "bFr";
+            File::create(FILENAME)?;
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(FILENAME)
+                .unwrap();
+            if let Err(e) = writeln!(
+                file,
+                "{}",
+                format!("==( logs )== \nstarted at : {:?} \n==( logs )==", local_time)
+            ) {
+                eprintln!("Couldn't write to file: {}", e);
+            }
         }
-        if data.data[i].finished {
-            _finished_style = "bFg";
-        } else {
-            _finished_style = "bFr"
-        }
-        table.add_row(Row::new(vec![
-            Cell::new(&(i + 1).to_string()).style_spec(&index_style),
-            Cell::new(&data.data[i].username),
-            Cell::new(&data.data[i].password),
-            Cell::new(&(data.data[i].status).to_string())
-                //
-                .style_spec(_status_style),
-            Cell::new(&(data.data[i].finished).to_string())
-                //
-                .style_spec(_finished_style),
-        ]));
+        thread::sleep(DURATION);
     }
-    // Print the table to stdout
-    table.printstd();
-    Ok(())
-}
-
-fn parse(filename: &str) -> JsonData {
-
-    let contents = fs::read_to_string(filename)
-        //
-        .expect("Something went wrong reading the file");
-    let data: JsonData = serde_json::from_str(&contents).expect(&format!(
-        "Failed to parse json file, Make sure the json file is correct ({})", filename
-    ));
-    data
 }
